59bfc34d9fffde9e1e641a57d50a5c84152fa661
Petter Rasmussen <petter.rasmussen@gmail.com>
2014-05-17 00:25:44 +0200

    Add more actions to controller template

--- templates/controller/controller.php
+++ templates/controller/controller.php
@@ -2,6 +2,22 @@
 
 class {{className}} extends {{parentClassName}} {
     public function indexAction() {
+        $this->loadLayout()->renderLayout();
+    }
 
+    public function jsonAction() {
+        $data = json_encode(array('foo' => 'bar'));
+        $this->getResponse()->setHeader('Content-type', 'application/json');
+        $this->getResponse()->setBody($data);
+    }
+
+    public function htmlAction() {
+        $html = $this->getLayout()
+            ->createBlock('modulename/foo')
+            ->setTemplate('modulename/foo.phtml')
+            ->toHtml();
+
+        $this->getResponse()->setHeader('Content-type', 'text/html');
+        $this->getResponse()->setBody($html);
     }
 }
