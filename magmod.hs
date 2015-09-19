9c498b1365f109595b288a79de9a42cf62bcde21
Petter Rasmussen <petter.rasmussen@gmail.com>
2015-09-19 13:50:41 +0200

    Version 1.0.1

--- magmod.hs
+++ magmod.hs
@@ -188,7 +188,7 @@ generateLayoutHandler scope =
 versionHandler :: IO ()
 versionHandler = do
     name <- getProgName
-    putStrLn $ name ++ " " ++ "1.0.0"
+    putStrLn $ name ++ " " ++ "1.0.1"
 
 cleanHandler :: IO ()
 cleanHandler = do
